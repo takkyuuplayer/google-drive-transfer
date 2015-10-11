87fb0a4667696d28f90c160277e3a22b02897b85
takkyuuplayer <takkyuuplayer@gmail.com>
2015-10-12 01:06:10 +0900

    Add spreadsheet logging

--- transfer.js
+++ transfer.js
@@ -3,6 +3,27 @@ var SOURCE_EMAIL = "source@gmail.com";
 
 var DESTINATION_FOLDER = "migratition-destination";
 
+var log = (function() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  sheet.deleteRows(2, sheet.getLastRow()-1);
+  sheet.getRange('A1:D1').setValues([
+      ['documentId', 'documentName', 'Source URL', 'STATUS']
+  ]);
+
+  var curRow = 2;
+  return function(file, status, message) {
+    message = message || "";
+    sheet.appendRow([
+        file.getId(),
+        file.getName(),
+        file.getUrl(),
+        status,
+        message
+    ]);
+    curRow++;
+  };
+})();
+
 
 function main () {
   var dest = destinationFolder();
@@ -12,28 +33,6 @@ function main () {
     var folder = folders.next();
     copy(folder, dest);
     break;
-  }
-}
-
-function copy (srcDir, destDir) {
-  var files = srcDir.getFiles();
-  while (files.hasNext()) {
-    var file = files.next();
-    if(file.getOwner().getEmail().toLowerCase() !== SOURCE_EMAIL.toLowerCase()) continue;
-
-    try {
-      file.makeCopy(file.getName(), destDir);
-    } catch (error) {
-      Logger.log("Copy failure: " + file.getName() + " !!!!");
-      Logger.log(error.message);
-    }
-  }
-
-  var dirs = srcDir.getFolders();
-  while (dirs.hasNext()) {
-    var src = dirs.next();
-    var dest = destDir.createFolder(src.getName());
-    copy(src, dest)
   }
 }
 
@@ -51,3 +50,34 @@ function destinationFolder() {
   return destFolderDir;
 }
 
+function copy (srcDir, destDir) {
+  var files = srcDir.getFiles();
+  while (files.hasNext()) {
+    var file = files.next();
+    if(file.getOwner().getEmail().toLowerCase() !== SOURCE_EMAIL.toLowerCase()) continue;
+
+    var tryCount = 1;
+    while(1) {
+      try {
+        file.makeCopy(file.getName(), destDir);
+        log(file, "OK");
+        break;
+      } catch (error) {
+        if(tryCount++ > 3) {
+          log(file, "NG", error.message);
+        } else {
+          Logger.log(file.getName());
+          Logger.log(error.message);
+        }
+        Utilities.sleep(1000);
+      }
+    }
+  }
+
+  var dirs = srcDir.getFolders();
+  while (dirs.hasNext()) {
+    var src = dirs.next();
+    var dest = destDir.createFolder(src.getName());
+    copy(src, dest)
+  }
+}
